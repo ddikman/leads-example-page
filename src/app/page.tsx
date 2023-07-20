af427a090d4915a469ce8ebba8e5d5f3a25f48cb
David Dikman <ddikman@gmail.com>
2023-07-20 22:18:51 +0900

    Some style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,20 +15,22 @@ interface Props {
   } | undefined
 }
 
+const PAGE_SIZE = 25
+
 export default async function Home({ searchParams }: Props) {
   const page = searchParams?.page ? Number(searchParams?.page) : 1
   const allPosts = await getPosts();
-  const posts = allPosts.slice((page - 1) * 10, page * 10);
+  const posts = allPosts.slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE);
 
-  const nextPage = (page * 10) >= allPosts.length ? null : page + 1
+  const nextPage = (page * PAGE_SIZE) >= allPosts.length ? null : page + 1
   const prevPage = page > 1 ? page - 1 : null
 
   return (
     <div>
-      <h2>Recent posts</h2>
+      <h2 className="text-2xl mb-4">Recent posts</h2>
       {posts.map((post: Post, index: number) => (<PostCard post={post} key={index} />))}
 
-      <div className='flex flex-row justify-between'>
+      <div className='flex flex-row justify-between px-2 mt-4'>
         { prevPage ? <Link className='prev-page' href={`?page=${prevPage}`}>Prev page</Link> : <span className='opacity-30'>Prev page</span>}
         { nextPage ? <Link className='next-page' href={`?page=${nextPage}`}>Next page</Link> : <span className='opacity-30'>Next page</span>}
       </div>
--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -47,8 +47,11 @@ export default async function Page({params} : { params: { id: string }}) {
   const user = await getUser(params.id);
   return <div>
     <div className="flex flex-row justify-between profile">
-      <div><span className="name">{ user.name }</span>, <span className="role">{ user.role }</span></div>
-      <Link href={`mailto:${user.email}`}>@contact</Link>
+      <div className="flex flex-col">
+        <span className="name text-lg">{ user.name }</span>
+        <span className="role">{ user.role }</span>
+      </div>
+      <Link href={`mailto:${user.email}`}>@email</Link>
     </div>
     <hr className="my-4 border-black border-dashed" />
     <div>Posts</div>
