fa498a829fbd1a7fdb359f35800f56df20b478fe
David Dikman <ddikman@gmail.com>
2023-07-20 22:29:34 +0900

    A bit more margin and spacing

--- src/app/post_card.tsx
+++ src/app/post_card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function PostCard({post} : { post: Post }) {
-  return <div className="flex flex-col my-2 w-full gap-2 rounded shadow-md bg-white p-2">
+  return <div className="flex flex-col mb-6 w-full gap-2 rounded shadow-md bg-white p-3">
     <div>{ post.message }</div>
     <div className="flex flex-row justify-between text-xs">
       <Link className="user-id" href={`/user/${post.user}`}>@{post.user}</Link>
