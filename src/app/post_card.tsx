98e72a6fb5181ef594364199e155d4bc7d4f1587
David Dikman <ddikman@gmail.com>
2023-07-20 12:12:11 +0900

    Updated sites with example content

---
+++ src/app/post_card.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function PostCard({post} : { post: Post }) {
+  return <div className="flex flex-col my-2 w-full gap-2 rounded shadow-md bg-white p-2">
+    <div>{ post.message }</div>
+    <div className="flex flex-row justify-between text-xs">
+      <Link className="user-id" href={`/user/${post.user}`}>@{post.user}</Link>
+      <span className="timestamp">{post.timestamp}</span>
+    </div>
+  </div>
+}
+
+
+export interface Post {
+  timestamp: String;
+  message: String;
+  user: String;
+}
---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,61 @@
+import path from 'path';
+import fs from 'fs/promises';
+import PostCard, { Post } from '../../post_card';
+import Link from 'next/link';
+
+async function getPosts() : Promise<Post[]> {
+  const jsonDirectory = path.join(process.cwd(), 'data');
+  const fileContents = await fs.readFile(jsonDirectory + '/posts.json', 'utf8');
+  return JSON.parse(fileContents) as Post[];
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  posts: Post[];
+}
+
+async function getUsers() : Promise<User[]> {
+  const jsonDirectory = path.join(process.cwd(), 'data');
+  const fileContents = await fs.readFile(jsonDirectory + '/people.json', 'utf8');
+  return JSON.parse(fileContents).map((user: any) => {
+    return {
+      ...user,
+      id: user['name'].replace(' ', '.').toLowerCase(),
+      posts: []
+    } as User
+  })
+}
+
+async function getUser(id: string) : Promise<User> {
+  const users = await getUsers();
+  const user = users.find((user: User) => user.id === id);
+
+  if (user == null) {
+    throw new Error('User not found')
+  }
+
+  const posts = await getPosts();
+  user.posts = posts.filter((post: Post) => post.user === id);
+
+  return user
+}
+
+export default async function Page({params} : { params: { id: string }}) {
+  const user = await getUser(params.id);
+  return <div>
+    <div className="flex flex-row justify-between">
+      <span>{ user.name }, { user.role }</span>
+      <Link href={`mailto:${user.email}`}>@contact</Link>
+    </div>
+    <hr className="my-4 border-black border-dashed" />
+    <div>Posts</div>
+    { user.posts.map((post: Post, index: number) => (<PostCard post={post} key={index} />)) }
+
+    <div className='mt-8'>
+      <Link href='/'>Back to top</Link>
+    </div>
+  </div>
+}
